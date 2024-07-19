d5a89e1f99ea0d093507da19501ec562a69d42b6
jose.tunon@itxpertix.com <jose.tunon@itxpertix.com>
2024-07-19 13:11:52 -0500

    nueva conexion

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,10 @@
 module.exports = {
   normal: {
-    host: "monorail.proxy.rlwy.net",
+    host: "roundhouse.proxy.rlwy.net",
     user: "root",
     password: "D6A24DCHCDe-ehfF6BFG5baC4-G3-f-5",
     database: "railway",
-    port: 28083
+    port: 18259
   },
 };
 
